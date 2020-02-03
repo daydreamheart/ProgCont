3568012f8498ae77b1884d5cbf58984cd357154a
Melinda <m4gy4r.m3l1nd4@gmail.com>
2020-02-03 14:54:02 +0100

    Add files via upload

---
+++ Magasszintű programozási nyelvek 1/2014. március 12., Sz18/relativprimek.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+// @author Magyar Melinda Barbara
+
+int main () {
+
+    int number1, number2, divider = 0;
+
+    while(scanf("%d %d", &number1, &number2) != EOF) {
+        while(number2 && number1 > 0) {
+            divider = number2;
+            number2 = number1 % number2;
+            number1 = divider;
+        }
+
+       if (divider == 1)
+            printf("YES\n");
+        else
+            printf("NO\n");
+       }
+
+    return 0;
+}
